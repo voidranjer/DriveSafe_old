cc72bbfb5252b303ba8a8a97287ccb098ac4fdd2
James <contact@jamesyap.org>
2024-04-27 14:24:17 -0400

    Untested: Added LSTM

--- utils/data.py
+++ utils/data.py
@@ -45,3 +45,35 @@ def get_labeled_data(data_dir: str, validation_dir: str, LABELS: set):
     validation_labels = labels["validation"]
 
     return training_data, training_labels, validation_data, validation_labels
+
+
+def get_sequence_data(dataset_path: str, target_labels: set, num_frames: int):
+    data = {
+        "training": [],
+        "validation": []
+    }
+
+    labels = {
+        "training": [],
+        "validation": []
+    }
+    
+    for mode in ["training", "validation"]:
+        for label in target_labels:
+            image_paths = list(paths.list_images(os.path.join(dataset_path, mode, label)))
+
+            # Read the images and create a sequence
+            sequence = []
+            for img_path in sorted(image_paths):
+                image = cv2.imread(img_path)
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # Is this really necessary??
+                image = cv2.resize(image, (224, 224))  # Resize image if necessary
+                sequence.append(image)
+                
+                # If we have enough frames, append the sequence to the data and labels
+                if len(sequence) == num_frames:  # Assuming N frames per sequence
+                    data[mode].append(sequence)
+                    labels[mode].append(label)
+                    sequence = []
+    
+    return data, labels
